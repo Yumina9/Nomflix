d84111a6cd782efff83c696eed5e79ead499b100
Yumina9 <saei5455@naver.com>
2020-05-10 20:25:24 +0900

    Add Todo for TabComponent

--- src/Components/TabComponent.js
+++ src/Components/TabComponent.js
@@ -2,7 +2,15 @@ import React from "react";
 import { Tab, TabList, Tabs, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
+// TODO
+// tv API에 에피소드 받아오는 API 추가하기
+// 라이프사이클 이해하기
+// https://velog.io/@velopert/react-hooks
+// 위 내용 활용해 적용해보기 컴포넌트에
+
 const DetailComponents = ({ result }) => {
+  // /tv/{tv_id}/season/{season_number}
+
   return (
     <Tabs>
       <TabList>
